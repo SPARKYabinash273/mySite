d8cb4aa6e1d4bb733e7efc9928c311e482e43702
SPARKYabinash273 <64353169+SPARKYabinash273@users.noreply.github.com>
2020-11-19 23:14:05 +0530

    Final commit

---
+++ src/resumeData.js
@@ -0,0 +1,113 @@
+let resumeData = {
+  name: "Abinash Seth",
+  role: "Fullstack Developer",
+  linkedinId: "Your LinkedIn Id",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies and play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/abinash-seth273",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/SPARKYabinash273",
+      className: "fa fa-github",
+    },
+    {
+      name: "skype",
+      url: "http://twitter.com/AbinashSeth8",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    "I am currently a final year student at New Horizon College Of Engineering, pursuing MCA. I am a self taught Full Stack Web Developer, currently diving deeper into Data Structures & Algorithms. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "India",
+  website: "https://www.linkedin.com/in/abinash-seth273",
+  education: [
+    {
+      UniversityName: "New Horizon College of Engineering",
+      specialization: "Master in Computer Application",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2021",
+      Achievements: "Presented a Seminar on Analysis of Augmented Reality And Virtual Reality",
+    },
+    {
+      UniversityName: "Government Autonomous College,Rourkela",
+      specialization: "Electronic and Tele-Communication",
+      MonthOfPassing: "Apr",
+      YearOfPassing: "2018",
+      Achievements: "Made a Project on Smart Street Light System Using Ardrino in Final Year of Graduation",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
+    },
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
+    },
+  ],
+  skillsDescription: "",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+     skillname:"Python",
+  },
+  {
+  skillname:"java"
+  }
+  ],
+  portfolio: [
+    {
+      name: "Bike rental System",
+      description: "A small step toword computer based registration system",
+      imgurl: "images/portfolio/judah.jpg",
+    },
+    {
+      name: "Stagenography",
+      description: "This is the technique of hiding secret data within an ordinary, non-secret, file or message in order to avoid detection",
+      imgurl: "images/portfolio/origami.jpg",
+    },
+    {
+      name: "student portal",
+      description: "This is an Application where all the information of student related to the institute can be saved main feature of the application is to get notified the student regarding dues or any important circular",
+      imgurl: "images/portfolio/coffee.jpg",
+    },
+    {
+      name: "project 4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/console.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "Patience is a key element of success",
+      name: "Bill Gates",
+    },
+    {
+      description: "Failure is an option here. If things are not failing, you are not innovating enough.",
+      name: "Elon Musk  ",
+    },
+  ],
+};
+
+export default resumeData;
